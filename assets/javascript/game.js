142d020e84401fae086a82327d8dc77a82334017
cheebachong <atrodondi@gmail.com>
2020-02-04 15:37:19 -0800

    having trouble with object, but making headway on javas

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,49 @@
-var hangman = {
-  wins: 0,
-  currentWord: words[Math.floor(Math.random() * words.length)],
-  guessremaining: 15,
-  lettersguessed: [],
-  words: []
+var words = [
+  "Traffic",
+  "Memento",
+  "Magnolia",
+  "Monster",
+  "Trainspotting",
+  "Fargo",
+  "Batman",
+  "Inception",
+  "Platoon",
+  "Ratatouille",
+  "Superman",
+  "Dogma",
+  "Arrival",
+  "Scarface",
+  "Aliens",
+  "Halloween",
+  "Eraserhead",
+  "Jaws",
+  "Chinatown",
+  "Z"
+];
+var wins = 0;
+var guessesLeft = 15;
+var lettersGuessed = [""];
+var currentWord = words[Math.floor(Math.random() * words.length)];
+console.log(currentWord);
+
+document.onkeypress = function(event) {
+  var letter = event.key.toLowerCase();
+  console.log(letter);
+  //   getting the letter pressed
+
+  for (i = 0; i < currentWord.length; i++) {
+    if (currentWord[i] != letter) {
+      if (lettersGuessed.includes(letter)) {
+        return;
+      }
+      lettersGuessed.push(letter);
+      console.log(lettersGuessed);
+    }
+
+    //   if ( [currentWord].includes(letter) ){
+    //       document.getElementById("currentword").innerHTML
+    //   }
+
+    //   }
+  }
 };
---
+++ assets/javascript/objectgame.js
@@ -0,0 +1,31 @@
+var game = {
+  words: [
+    "Traffic",
+    "Memento",
+    "Magnolia",
+    "Monster",
+    "Trainspotting",
+    "Fargo",
+    "Batman",
+    "Inception",
+    "Platoon",
+    "Ratatouille",
+    "Superman",
+    "Dogma",
+    "Arrival",
+    "Scarface",
+    "Aliens",
+    "Halloween",
+    "Eraserhead",
+    "Jaws",
+    "Chinatown",
+    "Z"
+  ],
+  wins: 0,
+  guessesLeft: 15
+};
+
+var currentword = game.words[Math.floor(Math.random() * words.length)];
+//   cant figure out how to make a property in the object refer to another
+// property which happens to be an array, maybe start without object
+// and see if i can make the game that way first.
